425ef3eaed6a646ec5d074f8a58ab6d4ea2c2777
honglunzhang <honglun-zhang@mthreads.com>
2023-09-26 13:57:30 +0800

    modify readme

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ cd ~/Desktop/mtai_workspace/MobiMaliangSDK
 apt-get install git -y
 apt-get install docker -y
 apt-get install docker.io -y
-docker login -u mcc-test@mcconline -p Abc123$%^ registry.mthreads.com
+docker login -u 账号 -p 密码 registry.mthreads.com
 docker pull registry.mthreads.com/mcconline/musa-pytorch-release-public:latest
 docker run -id --name mtai_workspace --privileged -e MTHREADS_VISIBLE_DEVICES=all -p 1001:1001 -p 1002:1002 -p 1003:1003 -p 1004:1004 -p 1005:1005 -v ~/Desktop/mtai_workspace:/mtai_workspace:rw --shm-size 64G registry.mthreads.com/mcconline/musa-pytorch-release-public:latest
 docker exec -it mtai_workspace /bin/bash
